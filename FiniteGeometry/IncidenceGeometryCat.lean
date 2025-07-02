31c05ed76fa85b6edb9ad01fc281a630e6b53c47
Javier Burroni <javier.burroni@gmail.com>
2025-07-02 09:47:08 -0400

    initial version of IncidenceGeometry

---
+++ FiniteGeometry/IncidenceGeometryCat.lean
@@ -0,0 +1,58 @@
+import Mathlib.CategoryTheory.Category.Basic
+
+
+universe u
+
+structure IncidenceGeometry where
+  Point : Type u
+  Line : Type u
+  incidence : Point → Line → Prop
+
+namespace IncidenceGeometry
+
+section Category
+open CategoryTheory
+
+@[ext]
+structure IncidenceHom (G H : IncidenceGeometry.{u}) where
+  pointMap : G.Point → H.Point
+  lineMap : G.Line → H.Line
+  preserves_incidence : ∀ {p : G.Point} {ℓ : G.Line},
+    G.incidence p ℓ → H.incidence (pointMap p) (lineMap ℓ)
+
+namespace IncidenceHom
+
+def id (G : IncidenceGeometry) : IncidenceHom G G where
+  pointMap := fun p ↦ p
+  lineMap := fun ℓ ↦ ℓ
+  preserves_incidence := fun h ↦ h
+
+def comp {G H K : IncidenceGeometry}
+  (f : IncidenceHom G H) (g : IncidenceHom H K) : IncidenceHom G K where
+    pointMap := g.pointMap ∘ f.pointMap
+    lineMap := g.lineMap ∘ f.lineMap
+    preserves_incidence := fun h ↦
+      g.preserves_incidence (f.preserves_incidence h)
+
+end IncidenceHom
+
+instance : Category IncidenceGeometry.{u} where
+  Hom := IncidenceHom
+  id := IncidenceHom.id
+  comp := IncidenceHom.comp
+
+structure Iso (G H : IncidenceGeometry) extends IncidenceHom G H where
+  inv : IncidenceHom H G
+  left_inv : ∀ p : G.Point, inv.pointMap (pointMap p) = p
+  right_inv : ∀ q : H.Point, pointMap (inv.pointMap q) = q
+  left_inv_line : ∀ l : G.Line, inv.lineMap (lineMap l) = l
+  right_inv_line : ∀ m : H.Line, lineMap (inv.lineMap m) = m
+
+
+def dual (G : IncidenceGeometry) : IncidenceGeometry where
+  Point := G.Line
+  Line := G.Point
+  incidence := fun l p ↦ G.incidence p l
+end Category
+
+end IncidenceGeometry
