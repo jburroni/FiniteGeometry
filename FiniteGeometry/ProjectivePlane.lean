ac735c056c9c46475b88effb277723f6b920e59c
Javier Burroni <javier.burroni@gmail.com>
2025-07-14 15:12:37 -0400

    remove unused lemmas

--- FiniteGeometry/ProjectivePlane.lean
+++ FiniteGeometry/ProjectivePlane.lean
@@ -29,31 +29,6 @@ class ProjectivePlane (G : IncidenceGeometry) : Prop where
         ℓ ≠ m ∧ ℓ ≠ n ∧ m ≠ n ∧
         G.incidence p ℓ ∧ G.incidence p m ∧ G.incidence p n
 
-namespace ProjectivePlane
-
-variable {G : IncidenceGeometry} [ProjectivePlane G]
-
-noncomputable
-def lineThrough {p q : G.Point} (h : p ≠ q) : G.Line :=
-  (P1 h).choose
-
-lemma lineThrough_incidence_left {p q : G.Point} (h : p ≠ q) : G.incidence p (lineThrough h) :=
-  (P1 h).choose_spec.1.1
-
-lemma lineThrough_incidence_right {p q : G.Point} (h : p ≠ q) : G.incidence q (lineThrough h) :=
-  (P1 h).choose_spec.1.2
-
-noncomputable
-def meet {ℓ m : G.Line} (h : ℓ ≠ m) : G.Point :=
-  (P2 h).choose
-
-lemma meet_incidence_left {ℓ m : G.Line} (h : ℓ ≠ m) : G.incidence (meet h) ℓ :=
-  (P2 h).choose_spec.1.1
-
-lemma meet_incidence_right {ℓ m : G.Line} (h : ℓ ≠ m) : G.incidence (meet h) m :=
-  (P2 h).choose_spec.1.2
-
-end ProjectivePlane
 
 instance dual_projective (G : IncidenceGeometry) [ProjectivePlane G] : ProjectivePlane G.dual where
   P1 := ProjectivePlane.P2
