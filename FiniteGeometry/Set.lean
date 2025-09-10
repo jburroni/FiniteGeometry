bd0ea607b3a4e741b18b1700b53b7e615ed40788
Javier Burroni <javier.burroni@gmail.com>
2025-09-10 16:33:07 -0400

    improve subset_four_choose_three

--- FiniteGeometry/Set.lean
+++ FiniteGeometry/Set.lean
@@ -4,34 +4,23 @@ import Mathlib.Data.Set.Insert
 namespace Set
 variable {α : Type*}
 
+-- increase heartbeats in the following lemma
+set_option maxHeartbeats 500000 in
 lemma subset_four_choose_three  {A B C D A' B' C' : α} {T : Set α} (h : T = {A', B', C'})
     (h₁ : A' ≠ B' ∧ A' ≠ C' ∧ B' ≠ C') : T ⊆ {A, B, C, D} → (T = {A, B, C}) ∨
     (T = {A, B, D}) ∨ (T = {A, C, D}) ∨ (T = {B, C, D}) := by
   subst T
-  obtain ⟨_, _, _⟩ := h₁
-  intro h₂
-  have := Set.subset_def.mp h₂
-  simp at this
-  rcases this with ⟨(rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl)⟩
+  intro h₂; simp [Set.subset_def] at h₂
+  -- each of A', B', C' can take any of 4 values.
+  -- we analyze each of the 64 combinations;
+  rcases h₂ with ⟨(rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl)⟩
   any_goals
     first
-    | contradiction
-    | simp
-  any_goals
-    first
+    | tauto
     | right; right; right; ext x; simp; tauto
     | right; right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · right; left; ext x
-    simp [or_left_comm, or_comm, or_assoc]
+    | right; left; ext x; simp; tauto
+    | left; ext x; simp; tauto
 
 
 
