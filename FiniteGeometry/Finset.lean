e764727d5e385c24a3095440b1b33c010bf5a868
Javier Burroni <javier.burroni@gmail.com>
2025-09-10 09:11:50 -0400

    moved Set operations to Set

--- FiniteGeometry/Finset.lean
+++ FiniteGeometry/Finset.lean
@@ -17,39 +17,3 @@ lemma card_finset_three {A B C : α} (hAB : A ≠ B) (hAC : A ≠ C)
 
 
 end Finset
-
-namespace Set
-variable {α : Type*}
-
-lemma subset_four_choose_three  {A B C D A' B' C' : α} {T : Set α} (h : T = {A', B', C'})
-    (h₁ : A' ≠ B' ∧ A' ≠ C' ∧ B' ≠ C') : T ⊆ {A, B, C, D} → (T = {A, B, C}) ∨
-    (T = {A, B, D}) ∨ (T = {A, C, D}) ∨ (T = {B, C, D}) := by
-  subst T
-  obtain ⟨_, _, _⟩ := h₁
-  intro h₂
-  have := Set.subset_def.mp h₂
-  simp at this
-  rcases this with ⟨(rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl), (rfl | rfl | rfl | rfl)⟩
-  any_goals
-    first
-    | contradiction
-    | simp
-  any_goals
-    first
-    | right; right; right; ext x; simp; tauto
-    | right; right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · right; left; ext x
-    simp [or_left_comm, or_comm, or_assoc]
-
-
-
-end Set
