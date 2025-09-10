fcf35767c312da94e550acccd3321faf9f9482c8
Javier Burroni <javier.burroni@gmail.com>
2025-09-10 08:40:40 -0400

    issue with any_goals and first

--- FiniteGeometry/Finset.lean
+++ FiniteGeometry/Finset.lean
@@ -34,26 +34,35 @@ lemma subset_four_choose_three  {A B C D A' B' C' : α} {T : Set α} (h : T = {A
     first
     | contradiction
     | simp
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · right; right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext y; simp; tauto
-  · left; ext x; simp; tauto
-  · right; left; ext x; simp; tauto
-  · right; right; right; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; right; left; ext x; simp; tauto
-  · left; ext x; simp; tauto
-  · right; right; right; ext x; simp; tauto
-  · right; right; left; ext y; simp; tauto
-  · right; right; right; ext z; simp; tauto
-  · right; left; ext x; simp; tauto
-  · right; right; left; ext y; simp; tauto
-  · right; left; ext z; simp; tauto
-  · right; right; right; ext w; simp; tauto
-  · right; right; left; ext y; simp; tauto
-  · right; right; right; ext z; simp; tauto
+  any_goals
+    first
+    | right; right; right; ext x; simp; tauto
+    | right; right; left; ext x; simp; tauto
+
+    -- | right; left; ext x; simp; tauto
+    -- | left; ext x; simp; tauto
+    -- | exfalso; contradiction
+  -- | right; left; ext x; simp; tauto
+  -- · left; ext x; simp; tauto
+  -- · right; left; ext x; simp; tauto
+  -- · right; right; left; ext x; simp; tauto
+  -- · left; ext x; simp; tauto
+  -- · right; left; ext y; simp; tauto
+  -- · left; ext x; simp; tauto
+  -- · right; left; ext x; simp; tauto
+  -- · right; right; right; ext x; simp; tauto
+  -- · left; ext x; simp; tauto
+  -- · right; right; left; ext x; simp; tauto
+  -- · left; ext x; simp; tauto
+  -- · right; right; right; ext x; simp; tauto
+  -- · right; right; left; ext y; simp; tauto
+  -- · right; right; right; ext x; simp; tauto
+  -- · right; left; ext x; simp; tauto
+  -- · right; right; left; ext y; simp; tauto
+  -- · right; left; ext z; simp; tauto
+  -- · right; right; right; ext w; simp; tauto
+  -- · right; right; left; ext x; simp; tauto
+  -- · right; right; right; ext z; simp; tauto
 
 
 end Set
